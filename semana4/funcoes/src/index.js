4864ee6d3d20f8441a7962894f6bd6262ec75375
RicardoMejolaro <56407810+RicardoMejolaro@users.noreply.github.com>
2020-10-05 16:01:05 -0300

    exercício 4b utilizando variáveis ao invés dos parametros

--- semana4/funcoes/src/index.js
+++ semana4/funcoes/src/index.js
@@ -115,22 +115,24 @@ multiplyParNumbers().*/
 // whoAmI()
 
 //4.B
-// function personInformation(name, age, city, student) {
-//   if (student) {
-//     student = "sou"
-//   } else {
-//     student = "não sou"
-//   }
+function personInformation(name, age, city, student) {
+  let studentCheck
+  let cityCheck
+  if (student) {
+    studentCheck = "sou"
+  } else {
+    student = "não sou"
+  }
 
-//   if (city === "São Paulo") {
-//     city = "Rua Guarapari 190"
-//   } else {
-//     city = city
-//   }
+  if (city === "São Paulo") {
+    cityCheck = "Rua Guarapari 190"
+  } else {
+    cityCheck = city
+  }
 
-//   return `Eu sou ${name}, tenho ${age} anos, moro em ${city} e ${student} estudante`
-// }
-// console.log(personInformation("Goli", 23, "São Paulo", true))
+  return `Eu sou ${name}, tenho ${age} anos, moro em ${cityCheck} e ${studentCheck} estudante`
+}
+console.log(personInformation("Goli", 23, "São Paulo", true))
 
 //EXERCÍCIO 5:
 //5.A
