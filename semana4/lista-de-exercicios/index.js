e431d2b58609a462bb3fd4493c3ccd35e5cfb854
RicardoMejolaro <ricardo.mejolaro@gmail.com>
2020-10-09 11:32:20 -0300

    Exercicio de interpretacao de codigo

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,49 @@
+/*==========================Exercícios de interpretação de código===============================*/
+//EXERCÍCIO 1:
+/*A função conversor de moeda é, a conversão de valor em dólar para valor em real, o
+retorno neste caso será: R$ (valor em real) dependendo do valor de cotação informado. */
+
+//EXERCÍCIO 2:
+/*A função investe dinheiro, calcula o retorno financeiro dependendo do tipo de 
+investimento junto ao valor investido. 
+No caso do investimento novoMontante o retorno será: 165.
+No caso do investimento segundoMontante o retorno será: TIPO DE INVESTIMENTO INFORMADO INCORRETO!
+*/
+
+//EXERCÍCIO 3:
+/*O código 3 analisa um array de números e retorna no array1 os números pares e no array2
+os números ímpares. Os retornos dos consoles são:
+Quantidade total de números é: 14
+6
+8
+*/
+
+//EXERCÍCIO 4:
+/*O código 4 analisa o array inteiro e no primeiro if o numero1 só recebe o numero quando o numero 
+for menor que numero1 então ele segue a sequência: 
+numero1 recebe 25, pois, é menor que infinito(porpriedade do Javascript).
+numero1 recebe 12, pois, é menor que 25.
+numero1 não recebe: 55, 64, 121, 44, pois, não são menores que 12.
+numero1 recebe 11, pois, é menor que 12.
+numero1 não recebe: 84, 51, 48, 14, 73, 111, 283, pois, não são menores que 11.
+numero1 recebe 1, pois, é menor que 11.
+numero1 não recebe: 99, 13, 31, 83, 131, 1, 1.1, pois, não são menores que 1.
+numero1 recebe -10, pois, é menor que 1.
+numero1 não recebe: 25, e 1590, pois, não são menores que -10.
+
+No segundo if o numero2 só recebe o numero quando o numero for maior que numero2 então ele segue
+a sequência:
+numero2 recebe 25, pois, é maior que 0.
+numero2 não recebe 12, pois, não é maior que 25.
+numero2 recebe 55, pois, é maior que 25.
+numero2 recebe 64, pois, é maior que 55.
+numero2 recebe 121, pois, é maior que 64.
+numero2 não recebe: 44, 11, 84, 51, 48, 14, 73, 111, pois, não são maiores que 121.
+numero2 recebe 283, pois, é maior que 121.
+numero2 não recebe: 1, 99, 13, 31, 83, 131, 1, 1.1, -10, 25, pois, não são maiores que 283.
+numero2 recebe 1590, pois, é maior que 283.
+
+Impressões do console são: -10 e 1590.
+*/
+
+
